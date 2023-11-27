454cea8e8f538637b105879d3bbeb61d8ceae155
Elias Lorandi <elias.lorandi@hotmail.com>
2023-11-27 17:06:44 -0300

    Criada algumas views

---
+++ resources/views/contatos.blade.php
@@ -0,0 +1,7 @@
+@extends('master')
+
+@section('content')
+
+<h2>CONTATOS</h2>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContatoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/contatos', [ContatoController::class, 'index']);
