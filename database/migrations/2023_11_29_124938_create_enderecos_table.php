fc3eada17a841e4bbc8e12b19bb5e962f71af35e
Elias Lorandi <elias.lorandi@hotmail.com>
2023-11-29 09:50:43 -0300

    Criação da tabela de endereços

--- database/migrations/2023_11_29_124938_create_enderecos_table.php
+++ database/migrations/2023_11_29_124938_create_enderecos_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('enderecos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('contato_id');
+            $table->string('cep')->nullable();
+            $table->string('rua')->nullable();
+            $table->integer('numero')->nullable();
+            $table->string('complemento')->nullable();
+            $table->string('bairro')->nullable();
+            $table->string('cidade')->nullable();
+            $table->string('estado')->nullable();
             $table->timestamps();
+    
+            $table->foreign('contato_id')->references('id')->on('contatos')->onDelete('cascade');
         });
     }
 
